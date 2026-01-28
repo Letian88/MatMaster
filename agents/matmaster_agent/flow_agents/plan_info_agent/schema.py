88c50c749c0f51853c7c7a7a8785c787705ca310
Rasic2 <1051987201@qq.com>
2026-01-28 11:47:54 +0800

    refactor：修改计划格式和架构以支持结构化步骤列表

--- agents/matmaster_agent/flow_agents/plan_info_agent/schema.py
+++ agents/matmaster_agent/flow_agents/plan_info_agent/schema.py
@@ -3,7 +3,17 @@ from typing import List
 from pydantic import BaseModel
 
 
+class PlanInfoSinglePlanStepsSchema(BaseModel):
+    tool_name: str
+    step_description: str
+
+
+class PlanInfoSinglePlanSchema(BaseModel):
+    plan_description: str
+    steps: List[PlanInfoSinglePlanStepsSchema]
+
+
 class PlanInfoSchema(BaseModel):
     intro: str
-    plans: List[str]
+    plans: List[PlanInfoSinglePlanSchema]
     overall: str
